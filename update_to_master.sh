f0e21408729bbf61992ab2981ea75f126c09ecd9
Jim Klassen <klassen.js@gmail.com>
2014-09-13 15:20:16 -0500

    Script to update geomoose/geomoose to master versions of submodules.

---
+++ update_to_master.sh
@@ -0,0 +1,29 @@
+# Copyright (c) 2009-2012, Dan "Ducky" Little & GeoMOOSE.org
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This script is meant to update all the submodules of geomoose/geomoose to 
+# point to the head of their master branch.
+# This has the effect of updating the demo.
+
+git checkout master && \
+git submodule foreach 'git checkout master && git pull' && \
+git commit -a -m 'Update submodules to master'
+
+echo "Remember to push updates to github with \"git push\"."
